e56821641db49c334418dfd1df4154c549fcbde9
Diallo <maimounatou2002@gmail.com>
2024-12-22 18:09:19 +0100

    message

--- TPVikings/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/api/utils/database.php';
-
-try {
-    $db = getDatabaseConnection();
-    echo "Hello dear Viking!";
-} catch (PDOException $e) {
-    echo "Could not connect to the database. " . $e->getMessage();
-}
